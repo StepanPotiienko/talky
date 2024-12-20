41c1e40082947c3b06acfa5a204dbd05c7a8f084
StepanPotiienko <kotnexjieb@protonmail.com>
2024-12-20 07:56:42 +0200

    inited an empty React Native App

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native"
+
+const App = () => {
+    return (
+    <>
+        <AppHeader />
+    </>
+    )
+}
+
+const AppHeader = () => <div><Text>Header</Text></div>
+
+export default App
